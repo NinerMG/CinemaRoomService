b1c75f1ba69ac2cca1ba9e3e8bf59576f0643c65
NinerMG <maciej.gala@hotmail.com>
2024-10-27 18:06:06 +0100

    stepTwo calculate income

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         new CinemaSeats();
+        System.out.println();
+        new UserInput();
     }
 }
---
+++ src/UserInput.java
@@ -0,0 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInput {
+    CalculateIncome calculateIncome;
+    int rows;
+    int seats;
+    public UserInput(){
+        calculateIncome =  new CalculateIncome();
+        setNumbers();
+        printIncome();
+    }
+
+    private int getNumber(String message){
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        boolean isValid = false;
+
+        while (!isValid) {
+            System.out.println(message);
+            try {
+                number =  scanner.nextInt();
+                isValid = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter number!");
+                scanner.nextLine();
+            }
+        }
+        return number;
+    }
+
+    private void setNumbers(){
+         rows = getNumber("Enter the number of rows:");
+         seats = getNumber("Enter the number of seats in each row:");
+         calculateIncome.setsSeats(rows, seats);
+    }
+    private void printIncome(){
+        System.out.println("Total income:");
+        System.out.println("$" + calculateIncome.calculateIncomeSimple());
+    }
+}
